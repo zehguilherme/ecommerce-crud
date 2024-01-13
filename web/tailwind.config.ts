ba530c66b549662e8c6684e3961f16734fd059b3
José Guilherme <jgtomaine@hotmail.com>
2024-01-13 17:44:40 -0300

    Change project colors

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -6,14 +6,37 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+  theme:{
+    fontSize:{
+
     },
+    extend:{
+      colors:{
+        black:{
+          black6: '#000000',
+          black5: '#2B2F32',
+          black4: '#5E6366',
+          black3: '#83898C',
+          black2: '#ABAFB1',
+          black1: '#CFD3D4'
+        },
+        gray: {
+          gray1: '#DDE2E5',
+          gray2: '#D9D9D9'
+        },
+        red: {
+          red1: '#F57E77',
+          red2: '#FCF3F2'
+        },
+        green: '#32936F',
+        purple: '#5570F1',
+        blue: {
+          blue1: '#007BFF',
+          blue2: '#E9ECF8'
+        },
+        white: '#ffffff'
+      }
+    }
   },
   plugins: [],
 }
