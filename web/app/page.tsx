97e5f58cea5001106752d282825bfc00747be906
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2024-02-08 21:30:39 -0300

    Create search component and add to home page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,11 +1,23 @@
-import type { Metadata } from "next";
-import { Header } from "./components/Header";
+import type { Metadata } from 'next';
+import { Header } from './components/Header';
+import { Search } from './components/inputs/Search';
 
 export const metadata: Metadata = {
-  title: "Home",
-  description: "E-commerce - Gerenciamento de produtos",
+  title: 'Home',
+  description: 'E-commerce - Gerenciamento de produtos',
 };
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+
+      <main className="px-6 py-8">
+        <Search
+          label="Pesquisar"
+          placeholder="Procurando por algo específico?"
+        />
+      </main>
+    </>
+  );
 }
