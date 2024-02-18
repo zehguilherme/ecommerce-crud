26175fa981eec38ea6e2c0835d7e80edc55fe787
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2024-02-18 11:42:16 -0300

    Add eslint and prettier config; Change tailwind color name

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,16 +1,16 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="pt-br">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -6,22 +6,22 @@ const config: Config = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme:{
+  theme: {
     fontSize: {
-      sm: ["12px", "14.5px"],
-      base: ["14px", "16.9px"],
-      lg: ["16px", "19.4px"],
-      xl: ["20px", "24.2px"],
-      "2xl": ["22px", "27px"],
-      "3xl": ["24px", "29px"],
+      sm: ['12px', '14.5px'],
+      base: ['14px', '16.9px'],
+      lg: ['16px', '19.4px'],
+      xl: ['20px', '24.2px'],
+      '2xl': ['22px', '27px'],
+      '3xl': ['24px', '29px'],
     },
-    extend:{
-      colors:{
-        black:{
+    extend: {
+      colors: {
+        black: {
           black1: '#5E6366',
           black2: '#2B2F32',
           black3: '#130F26',
-          black4: '#000000'
+          black4: '#000000',
         },
         gray: {
           gray1: '#CFD3D4',
@@ -34,21 +34,21 @@ const config: Config = {
         },
         red: {
           red1: '#F57E77',
-          red2: '#FCF3F2'
+          red2: '#FCF3F2',
         },
         blue: {
           blue1: '#007BFF',
-          blue2: '#E9ECF8'
+          blue2: '#E9ECF8',
         },
         green: {
-          gree1: '#32936F',
-          gree2: '#E5F6ED',
+          green1: '#32936F',
+          green2: '#E5F6ED',
         },
         purple: '#5570F1',
-        white: '#ffffff'
-      }
-    }
+        white: '#ffffff',
+      },
+    },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
