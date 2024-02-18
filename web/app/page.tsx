2db7d3bab8a5d2f6ac4f00c50a7c08f730901fef
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2024-02-18 10:39:06 -0300

    Add a link that directs to the new product page

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -4,6 +4,8 @@ import { ChangeEvent, useState } from 'react';
 import { Header } from './components/Header';
 import { Search } from './components/inputs/Search';
 import { Select } from './components/inputs/Select';
+import Link from 'next/link';
+import { Plus } from './components/icons/Plus';
 
 export default function Home() {
   const [orderbySelectedOption, setOrderbySelectedOption] = useState('');
@@ -28,17 +30,27 @@ export default function Home() {
       <Header />
 
       <main className="px-6 py-8">
-        <Search
-          label="Pesquisar"
-          placeholder="Procurando por algo específico?"
-        />
+        <div className="flex flex-col gap-8">
+          <Search
+            label="Pesquisar"
+            placeholder="Procurando por algo específico?"
+          />
 
-        <Select
-          labelText="Ordenar por"
-          options={orderbyHtmlElementOptions}
-          onChange={handleOrderbyChange}
-          value={orderbySelectedOption}
-        />
+          <Select
+            labelText="Ordenar por"
+            options={orderbyHtmlElementOptions}
+            onChange={handleOrderbyChange}
+            value={orderbySelectedOption}
+          />
+
+          <Link
+            href={'/'}
+            className="capitalize bg-green-green1 px-4 py-[17px] rounded-xl w-full text-white font-normal text-xl flex justify-center items-center gap-5"
+          >
+            <Plus className="w-6 h-6" />
+            Adicionar produto
+          </Link>
+        </div>
       </main>
     </>
   );
