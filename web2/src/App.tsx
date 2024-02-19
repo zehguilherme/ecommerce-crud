bae3c621c07f8ba0fa8d591a4f02eea305162552
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2024-02-19 20:22:20 -0300

    Migrate previous NextJs project to ReactJs

--- web2/src/App.tsx
+++ web2/src/App.tsx
@@ -1,5 +1,57 @@
+import { ChangeEvent, useState } from "react";
+import { Header } from "./components/Header";
+import { Plus } from "./components/icons/Plus";
+import { Search } from "./components/inputs/Search";
+import { Select } from "./components/inputs/Select";
+import "./index.css";
+
 function App() {
-  return <h1>Hello World</h1>;
+  const [orderbySelectedOption, setOrderbySelectedOption] = useState("");
+
+  const orderbyHtmlElementOptions = [
+    {
+      value: "menor-valor",
+      label: "Menor Valor",
+    },
+    {
+      value: "maior-valor",
+      label: "Maior Valor",
+    },
+  ];
+
+  function handleOrderbyChange(event: ChangeEvent<HTMLSelectElement>) {
+    setOrderbySelectedOption(event.target.value);
+  }
+
+  return (
+    <>
+      <Header />
+
+      <main className="px-6 py-8">
+        <div className="flex flex-col gap-8">
+          <Search
+            label="Pesquisar"
+            placeholder="Procurando por algo específico?"
+          />
+
+          <Select
+            labelText="Ordenar por"
+            options={orderbyHtmlElementOptions}
+            onChange={handleOrderbyChange}
+            value={orderbySelectedOption}
+          />
+
+          <a
+            href="/"
+            className="capitalize bg-green-green1 px-4 py-[17px] rounded-xl w-full text-white font-normal text-xl flex justify-center items-center gap-5"
+          >
+            <Plus className="w-6 h-6" />
+            Adicionar produto
+          </a>
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default App;
---
+++ web2/src/components/Header.tsx
@@ -0,0 +1,15 @@
+import { Box } from "./icons/Box";
+
+export function Header() {
+  return (
+    <header className="bg-purple">
+      <nav className="flex justify-between items-center p-6 lg:px-0 max-w-[1200px] mx-auto">
+        <a href="/">
+          <Box className="text-white" />
+        </a>
+
+        <span className="text-3xl font-normal text-white">José Guilherme</span>
+      </nav>
+    </header>
+  );
+}
