7f5a8e64c827b4af97429ca3094813569080ea7a
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2024-06-18 21:09:15 -0300

    Change submit button state and color when the form can't be send

--- web/src/components/Button.tsx
+++ web/src/components/Button.tsx
@@ -2,15 +2,23 @@ type ButtonProps = {
   type: "button" | "submit" | "reset";
   text: string;
   className?: string;
+  disabled?: boolean;
   onClick?: () => void;
 };
 
-export function Button({ type, text, className, onClick }: ButtonProps) {
+export function Button({
+  type,
+  text,
+  className,
+  disabled,
+  onClick,
+}: ButtonProps) {
   return (
     <button
       type={type}
       className={`px-4 py-[17px] rounded-xl font-normal text-xl ${className}`}
       onClick={onClick}
+      disabled={disabled}
     >
       {text}
     </button>
