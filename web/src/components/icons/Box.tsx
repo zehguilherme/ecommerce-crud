64df2bf45a46d6bf2c08e4b6ddd6f1bdb636b909
José Guilherme Paro Monteiro Tomaine <jgtomaine@hotmail.com>
2024-06-25 19:46:50 -0300

    Fix fixed width and height from Box icon

--- web/src/components/icons/Box.tsx
+++ web/src/components/icons/Box.tsx
@@ -4,8 +4,6 @@ export function Box(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="32"
-      height="32"
       fill="none"
       viewBox="0 0 32 32"
       {...props}
